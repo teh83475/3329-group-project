850f2576fb08682dd5f55c7e6cc8b340db9ca2c5
teh83475 <tai621@connect.hku.hk>
2025-04-03 15:11:51 +0800

    feat: player animator

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+    Animator animator;
  
     public float speed = 12f;
     public float gravity = -9.81f * 2;
@@ -18,19 +21,29 @@ public class PlayerMovement : MonoBehaviour
  
     bool isGrounded;
     float sprintMultiplier = 1;
- 
+
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        
+        sprintMultiplier = 1;
+        animator.SetBool("isRunning", false);
+        animator.SetBool("isJumping", false);
         //checking if we hit the ground to reset our falling velocity, otherwise we will fall faster the next time
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
- 
+        
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
- 
+
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
  
@@ -41,10 +54,25 @@ public class PlayerMovement : MonoBehaviour
         if (!Input.GetKey(KeyCode.LeftShift) && isGrounded) {
             sprintMultiplier = 1;
         }
+        print(isGrounded);
+        
 
         //right is the red Axis, foward is the blue axis
         Vector3 move = transform.right * x + transform.forward * z;
- 
+
+        if (move.magnitude > 0) {
+            animator.SetBool("isRunning", true);
+  
+        }
+
+        if (!isGrounded)
+        {
+            animator.SetBool("isJumping", true);
+
+        }
+
+
+
         controller.Move(move * speed * sprintMultiplier * Time.deltaTime);
  
         //check if the player is on the ground so he can jump
