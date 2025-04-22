c5b73487273c723ab9708b7a9ccb167c1444873d
teh83475 <tai621@connect.hku.hk>
2025-04-22 20:51:05 +0800

    add bgm

--- Assets/Scripts/staff.cs
+++ Assets/Scripts/staff.cs
@@ -4,10 +4,15 @@ public class GuardianSpin : MonoBehaviour
 {
     public float spinSpeed = 30f; // Degrees per second
     public float cycleDuration;
+    public float startTime = 0;
 
     void Update()
     {
-        float cycleValue = Mathf.Repeat(Time.time, cycleDuration) < cycleDuration/2 ? -1f : 1f;
-        transform.Rotate(0, spinSpeed * Time.deltaTime * cycleValue, 0);
+        if (Time.time > startTime)
+        {
+            float cycleValue = Mathf.Repeat(Time.time - startTime, cycleDuration) < cycleDuration / 2 ? -1f : 1f;
+            transform.Rotate(0, spinSpeed * Time.deltaTime * cycleValue, 0);
+        }
+        
     }
 }
