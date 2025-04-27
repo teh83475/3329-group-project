6042f03c6677bc3be7dd5a6df8f95551ab634b9a
teh83475 <tai621@connect.hku.hk>
2025-04-27 23:48:09 +0800

    restart hotkey

--- Assets/Scripts/restart.cs
+++ Assets/Scripts/restart.cs
@@ -4,6 +4,14 @@ using System.Collections;
 
 public class Restart : MonoBehaviour
 {
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestartGame();
+        }
+
+    }
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
